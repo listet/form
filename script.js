cfecc4a417e30f39735459e6e1b8c07523455da6
Lisa Tetzlaff <lisa.emma.tetzlaff@gmail.com>
2024-01-25 12:07:28 +0100

    second commit

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 'use strict';
 
+
 window.addEventListener('load', initPage)
 
 function initPage() {
     // Gömma #registerForm och #contentContainer.
     document.querySelector('#registerForm').classList.add('d-none');
     document.querySelector('#contentContainer').classList.add('d-none');
+    document.querySelector('#loginForm').classList.remove('d-none');
 
     //Lägga till lyssnare på knapparna i formuläret och agera utifrån användarens knapptryck.
     let loginBtn = document.querySelector('#loginBtn');
-    loginBtn.addEventListener('click', () => {
+    loginBtn.addEventListener('click', (event) => {
         event.preventDefault();
         validateLogin()
     });
@@ -20,6 +22,7 @@ function initPage() {
         changeToRegister(event)
     });
 
+    logOut()
 }
 
 function validateLogin(event) {
@@ -111,8 +114,47 @@ function initContent() {
     document.querySelector('#loginForm').classList.add('d-none');
     document.querySelector('#registerForm').classList.add('d-none');
     document.querySelector('#contentContainer').classList.remove('d-none');
+
+    let eventLog = document.querySelector('#story');
+    let charactersCopy = [...characters];
+
+    function getRandomCharacter() {
+        if (charactersCopy.length === 0) {
+            // If all characters have been used, reset the copy
+            charactersCopy = [...characters];
+        }
+
+        let randomIndex = Math.floor(Math.random() * charactersCopy.length);
+        let randomCharacter = charactersCopy[randomIndex];
+        // Remove the used character from the copy
+        charactersCopy.splice(randomIndex, 1);
+
+        return randomCharacter;
+    }
+
+    let randomCharacter1 = getRandomCharacter();
+    let randomCharacter2 = getRandomCharacter();
+    let randomCharacter3 = getRandomCharacter();
+    let randomCharacter4 = getRandomCharacter();
+    let randomCharacter5 = getRandomCharacter();
+    let randomCharacter6 = getRandomCharacter();
+
+    eventLog.innerHTML += `One day ${randomCharacter1.Name} <img class='imgSize' src="${randomCharacter1.Image}" alt="${randomCharacter1.Name}"> `;
+    eventLog.innerHTML += `and his friend ${randomCharacter2.Name} <img class='imgSize' src="${randomCharacter2.Image}" alt="${randomCharacter2.Name}"> were out walking in the forest. `;
+    eventLog.innerHTML += `Suddenly ${randomCharacter3.Name} <img class='imgSize' src="${randomCharacter3.Image}" alt="${randomCharacter3.Name}"> appeared`;
+    eventLog.innerHTML += ` who brought news of an impending danger to the Mushroom Kingdom. ${randomCharacter1.Name} <img class='imgSize' src="${randomCharacter1.Image}" alt="${randomCharacter1.Name}"> and ${randomCharacter2.Name} <img class='imgSize' src="${randomCharacter2.Image}" alt="${randomCharacter2.Name}"> `;
+    eventLog.innerHTML += `knew they had to act quickly. With ${randomCharacter1.Name}'s jumping skills and ${randomCharacter2.Name}'s clever strategies, `;
+    eventLog.innerHTML += `they set out on a journey to gather allies, including ${randomCharacter5.Name} <img class='imgSize' src="${randomCharacter5.Image}" alt="${randomCharacter5.Name}">, `;
+    eventLog.innerHTML += `${randomCharacter6.Name} <img class='imgSize' src="${randomCharacter6.Image}" alt="${randomCharacter6.Name}"> and `;
+    eventLog.innerHTML += `${randomCharacter4.Name} <img class='imgSize' src="${randomCharacter4.Image}" alt="${randomCharacter4.Name}">.`;
+
 }
 
-function logOut() {
 
+function logOut() {
+    let logoutBtn = document.querySelector('#logoutBtn');
+    logoutBtn.addEventListener('click', () => {
+        console.log('funkar?')
+        initPage()
+    });
 }
